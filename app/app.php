f6a83e9543a8af7243be7d1144b96f6c5c1b83b5
nic <netzeln@gmail.com>
2016-02-24 07:41:24 -0800

    update for databases

--- app/app.php
+++ app/app.php
@@ -2,9 +2,14 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/~~~~~~.php";
 
-    session_start();
+    // session_start();
 
     $app = new Silex\Application();
+
+    // $server = 'mysql:host=localhost;dbname=~~~~~';
+    // $username = 'root';
+    // $password = 'root';
+    // $DB = new PDO($server, $username, $password)
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
