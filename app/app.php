cf7f64aa7061e71bce25891ea5fffc1ca0b5561c
Erik Tolentino <tolentino3575@gmail.com>
2016-03-04 13:26:31 -0800

    Create twig files for index and stores

--- app/app.php
+++ app/app.php
@@ -1,12 +1,16 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/~~~~~~.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
 
     // session_start();
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost;dbname=~~~~~';
+    $server = 'mysql:host=localhost;dbname=shoes';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -14,7 +18,13 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
 
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
 
+    $app->get("/stores", function() use ($app){
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
 
     return $app;
  ?>
